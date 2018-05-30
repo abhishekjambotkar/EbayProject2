20d2cb421dc2a7595a04e93dc087d48fb091a926
abhishekjambotkar <abhishekjambotkar@gmail.com>
2018-05-30 16:38:17 +0530

    WithJavaCodingStandards

--- Practice_Project/src/com/Test/Ebay/TestProject.java
+++ Practice_Project/src/com/Test/Ebay/TestProject.java
@@ -43,75 +43,129 @@ public class TestProject {
 	
 		private static final int priority = 0;
 		@SuppressWarnings("rawtypes")
-		static AndroidDriver driver;
+		static
+		AndroidDriver driver;
 		private String searchedTvString = null;
 		private String searchedTvPrice = null;
-		FileInput files= new FileInput();
-		Ebay_LandingPage ebay_LandingPage = new  Ebay_LandingPage();
-		Ebay_SignInPage ebay_SignInPage = new Ebay_SignInPage();
-		Ebay_SignInConfirmationPage ebay_SignInConfirmationPage = new Ebay_SignInConfirmationPage();
-		Ebay_SearchScreen ebay_SearchScreen = new Ebay_SearchScreen();
-		Ebay_SearchResultsPage ebay_SearchResultsPage = new Ebay_SearchResultsPage();
-		Ebay_SearchedOptionPage ebay_SearchedOptionPage = new Ebay_SearchedOptionPage();
-		Ebay_ReviewPage ebay_ReviewPage = new Ebay_ReviewPage();
-		Ebay_CheckoutScreen ebay_CheckoutScreen = new Ebay_CheckoutScreen();
-		
+		private String selectedTvString = null;
+		private String selectedTvPrice = null;
+		private String selectedTvStringCompare = null;
+		private String searchedTvStringCompare = null;
+		private String checkoutScreen= "Delivery address";
+		private String priceToCompare= null; 
+
 		
 		
 		@BeforeClass
 		public static void setup() throws Exception
 		{
+			DesiredCapabilities caps = new DesiredCapabilities();
+			caps.setCapability("deviceName", "Ally galaxy 5");
+			caps.setCapability("udid", "3208a71d704361e1"); //Give Device ID of your mobile phone
+			caps.setCapability("platformName", "Android");
+			caps.setCapability("platformVersion", "6.0.1");
+			caps.setCapability("appPackage", "com.ebay.mobile");
+			caps.setCapability("appActivity", "com.ebay.mobile.activities.MainActivity");
+			caps.setCapability("noReset", "true");
 			
-			MobileDriverSetup mobiledriver = new MobileDriverSetup();
+		
 			
-			//Method to create the mobile driver session
-			mobiledriver.setup();
-			driver=mobiledriver.driver;
 			
+			driver = new AndroidDriver<MobileElement>(new URL("http://127.0.0.1:4723/wd/hub"), caps);
+			//driver=new AndroidDriver(new URL("http://127.0.0.1:4723/wd/hub"),caps);
+			System.out.println("app is launching");
 		}
 		
 		//@org.testng.annotations.Test
 		@Test
 		public void test_Scenario1_Pass() throws Exception {
 			
+			String FilePath = "D:\\Workspace\\UnifiedScripting\\Practice_Project\\data\\RDC.xls";// excel path
+			FileInputStream fs = new FileInputStream(FilePath);
+			Workbook wb = Workbook.getWorkbook(fs);
+			Sheet sh = wb.getSheet(0);//To get access of sheet1 
+			String Username = sh.getCell(0,1).getContents();
+			String Password = sh.getCell(1,1).getContents();
+			String SearchItem = sh.getCell(2,1).getContents();
+			System.out.println("Username: "+Username);
+			System.out.println("Password: "+Password);
+			System.out.println("SearchItem: "+SearchItem);
 			
-			//data from excel sheet
-			String username = files.Username();
-			String password = files.Password(); 
-			String searchItem = files.SearchItem();
 			
-			//Method to Sign In Page from Landing page
-			ebay_LandingPage.MoveToSigningPage(driver);
+			driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+			System.out.println("On Landing page");
 			
-			//Method to Enter the Username and password on Sign In Page
-			ebay_SignInPage.EnterDetailsOnSign(driver, username, password);
-		
-			//Method to confirm login
-			ebay_SignInConfirmationPage.SignInConfirmation(driver);
 			
-			//Method to Tap on SearchBox
-			ebay_LandingPage.TapOnSearchBox(driver);
-		
-			//Method to Enter value in SearchBox
-			ebay_SearchScreen.EnterSearchOption(driver, searchItem);
+			driver.findElement(By.id("com.ebay.mobile:id/home")).click();
 			
-			//Method to select an option from Searched Dropdown Menu
-			ebay_SearchScreen.SelectFromSearchDropBox(driver);
+			Thread.sleep(100);
+			driver.findElement(By.id("com.ebay.mobile:id/logo")).click();
 			
-			//Method to Select an option
-			ebay_SearchResultsPage.SelectAOption(driver);
+			Thread.sleep(2000);
+			System.out.println("On login page");
+			driver.findElement(By.id("com.ebay.mobile:id/edit_text_username")).sendKeys(Username);
+			Thread.sleep(2000);
+			driver.findElement(By.id("com.ebay.mobile:id/edit_text_password")).sendKeys(Password);
+			Thread.sleep(2000);
+			driver.findElement(By.id("com.ebay.mobile:id/button_sign_in")).click();
+			Thread.sleep(2000);
+			driver.findElement(By.name("No thanks")).click();
 			
-			//Method to obtain String details of selected device and Tap on Buy
-			ebay_SearchedOptionPage.SavingStringAndTapOnBuy(driver);
-			searchedTvString = ebay_SearchedOptionPage.searchedTvString;
-			searchedTvPrice = ebay_SearchedOptionPage.searchedTvPrice;
-		
-			//Method to obtain verify details of selected device on Review Screen and Tap on Review
-			ebay_ReviewPage.ComparingStringAndTapOnReview(driver, searchedTvString, searchedTvPrice);
+			Thread.sleep(2000);
+			driver.findElement(By.id("com.ebay.mobile:id/search_box")).click();
+			System.out.println("On Search Screen");
 			
-			//Method to obtain verify details of selected device on Checkout Screen and Tap on Review
-			ebay_CheckoutScreen.VerifiyingContentsOnCheckoutPage(driver, searchedTvString, searchedTvPrice);
+			Thread.sleep(100);
+			driver.findElement(By.name("Search for anything")).sendKeys(SearchItem);
+			System.out.println("Search Options");
 			
+			Thread.sleep(100);
+			driver.findElement(By.xpath("//android.widget.ListView[@index='1']/android.widget.RelativeLayout[@index='0']")).click();
+			System.out.println("Search Results");
+			
+			Thread.sleep(200);
+			driver.findElement(By.xpath("//android.support.v7.widget.RecyclerView[@index='0']/android.widget.RelativeLayout[@index='1']")).click();
+			System.out.println("Option Selected");
+			
+			Thread.sleep(200);
+			System.out.println("Selected Option");
+			searchedTvString= driver.findElement(By.id("com.ebay.mobile:id/textview_item_name")).getAttribute("text");
+			System.out.println(searchedTvString);
+			searchedTvPrice= driver.findElement(By.id("com.ebay.mobile:id/textview_item_price")).getAttribute("text");
+			System.out.println(searchedTvPrice);
+			driver.findElement(By.name("Buy it now")).click();
+			
+			Thread.sleep(200);
+			System.out.println("Selected Option Review Page");
+			selectedTvString= driver.findElement(By.id("com.ebay.mobile:id/item_title")).getAttribute("text");
+			System.out.println(selectedTvString);
+			selectedTvPrice= driver.findElement(By.id("com.ebay.mobile:id/textview_item_price")).getAttribute("text");
+			System.out.println(selectedTvPrice);
+			assertEquals(searchedTvString, selectedTvString);
+			assertEquals(searchedTvPrice, selectedTvPrice);
+			driver.findElement(By.name("Review")).click();
+			
+			Thread.sleep(10000);
+			//driver.swipe(560, 1064, 560, 992, 200);
+			 checkoutScreen=driver.findElement(By.name("Delivery address")).getAttribute("text");
+			 Thread.sleep(10000);
+			TouchAction ts = new TouchAction(driver);
+			ts.press(0, 1400).waitAction(400).moveTo(0,400).release().perform();
+			
+			selectedTvString= driver.findElement(By.xpath("//android.view.View[@index='3']/android.view.View[@index='2']/android.view.View[@index='1']/android.view.View[@index='1']/android.view.View[@index='0']/android.view.View[@index='0']")).getAttribute("text");
+			System.out.println(selectedTvString);
+			selectedTvPrice= driver.findElement(By.xpath("//android.widget.ListView[@index='3']/android.view.View[@index='3']")).getAttribute("text");
+			System.out.println(selectedTvPrice);
+			
+			selectedTvStringCompare=selectedTvString.trim();
+			searchedTvStringCompare=searchedTvString.trim();
+			assertEquals(searchedTvStringCompare, selectedTvStringCompare);
+			
+			priceToCompare = searchedTvPrice.substring(1);
+			System.out.println(priceToCompare);
+			priceToCompare = "Rs. "+priceToCompare+".00";
+			System.out.println(priceToCompare);
+			assertEquals(priceToCompare, selectedTvPrice);
 			
 			}
 		
